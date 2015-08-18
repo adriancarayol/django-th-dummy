41bbdf9d24bd54bee4566d61301f56b7f4683351
Olivier Demah <olivier@foxmask.info>
2015-08-18 14:42:48 +0200

    improvements of the dummy module

--- th_dummy/tests/test.py
+++ th_dummy/tests/test.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from django.test import TestCase
+from django.conf import settings
 from django.contrib.auth.models import User
 from th_dummy.models import Dummy
 from django_th.models import TriggerService, UserService, ServicesActivated
@@ -12,6 +13,9 @@ class DummyTest(TestCase):
         DummyTest Model
     """
     def setUp(self):
+        """
+           create a user
+        """
         try:
             self.user = User.objects.get(username='john')
         except User.DoesNotExist:
@@ -20,7 +24,11 @@ class DummyTest(TestCase):
 
     def create_triggerservice(self, date_created="20130610",
                               description="My first Service", status=True):
+        """
+           create a TriggerService
+        """
         user = self.user
+
         service_provider = ServicesActivated.objects.create(
             name='ServiceRSS', status=True,
             auth_required=False, description='Service RSS')
@@ -41,12 +49,18 @@ class DummyTest(TestCase):
                                              status=status)
 
     def create_dummy(self):
+        """
+            Create a dummy object related to the trigger object
+        """
         trigger = self.create_triggerservice()
         name = 'dummy'
         status = True
         return Dummy.objects.create(trigger=trigger, name=name, status=status)
 
     def test_dummy(self):
+        """
+           Test if the creation of the dummy object looks fine
+        """
         d = self.create_dummy()
         self.assertTrue(isinstance(d, Dummy))
         self.assertEqual(d.show(), "My Dummy %s" % (d.name))
@@ -56,22 +70,40 @@ class DummyTest(TestCase):
     """
     # provider
     def test_valid_provider_form(self):
+        """
+           test if that form is a valid provider one
+        """
         d = self.create_dummy()
         data = {'name': d.name}
         form = DummyProviderForm(data=data)
         self.assertTrue(form.is_valid())
 
     def test_invalid_provider_form(self):
+        """
+           test if that form is not a valid provider one
+        """
         form = DummyProviderForm(data={})
         self.assertFalse(form.is_valid())
 
     # consumer
     def test_valid_consumer_form(self):
+        """
+           test if that form is a valid consumer one
+        """
         d = self.create_dummy()
         data = {'name': d.name}
         form = DummyConsumerForm(data=data)
         self.assertTrue(form.is_valid())
 
     def test_invalid_consumer_form(self):
+        """
+           test if that form is not a valid consumer one
+        """
         form = DummyConsumerForm(data={})
         self.assertFalse(form.is_valid())
+
+    def test_get_config_th(self):
+        """
+            does this settings exists ?
+        """
+        self.assertTrue(settings.TH_DUMMY)
